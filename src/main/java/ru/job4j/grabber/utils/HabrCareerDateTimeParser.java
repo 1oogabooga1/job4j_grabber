1bf4d07057ebf15e3b25a639a59d27dde7f70c1f
1oogabooga1 <dmitrijdobrydin@gmail.com>
2024-03-23 20:47:13 +0300

    2.2. Модель данных - Post. [#285211 #487617]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -7,7 +7,6 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        OffsetDateTime offsetDateTime = OffsetDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        return offsetDateTime.toLocalDateTime();
+        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
 }
